a30216ce40098b14bd5860f6baf27d2b9ed43adf
Martin Heßmann <martin@apeunit.com>
2024-10-18 11:16:05 +0200

    added webfonts and removed node modules

--- craco.config.js
+++ craco.config.js
@@ -7,5 +7,30 @@ module.exports = {
       ],
     },
   },
-}
+  webpack: {
+    configure: (webpackConfig) => {
+      const fileLoaderRule = webpackConfig.module.rules.find(rule => rule.test && rule.test.test('.svg'));
+      fileLoaderRule.exclude = /\.svg$/;
 
+      webpackConfig.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+
+      webpackConfig.module.rules.push({
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      });
+
+      return webpackConfig;
+    },
+  },
+}
