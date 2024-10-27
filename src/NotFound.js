c7534ea74f9e6891239c532e310ba16a8f7bd5f7
Martin Heßmann <martin@apeunit.com>
2024-10-27 08:00:24 +0100

    revert redirects

--- src/NotFound.js
+++ src/NotFound.js
@@ -12,15 +12,15 @@ function NotFound() {
       textAlign: 'center',
       backgroundColor: '#091115',
       color: '#fff',
-      fontFamily: 'Arial, sans-serif',
     },
     title: {
-      fontSize: '3rem',
+      fontSize: '1.2rem',
       marginBottom: '1rem',
     },
     text: {
-      fontSize: '1.2rem',
+      fontSize: '1rem',
       marginBottom: '2rem',
+      lineHeight: '1.2',
     },
     button: {
       padding: '10px 20px',
