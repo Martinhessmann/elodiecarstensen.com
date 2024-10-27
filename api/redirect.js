0d00e81bc604cbfcfe170e5b0444fc9236e58c39
Martin Heßmann <martin@apeunit.com>
2024-10-27 07:35:38 +0100

    try to fix vercel build fail

--- api/redirect.js
+++ api/redirect.js
@@ -1,12 +1,10 @@
-export default function handler(request, response) {
-  const { url } = request.query;
+export default function handler(req, res) {
+  const { url } = req.query;
 
-  // Add your dynamic redirect logic here
   if (url && url.startsWith('/work/')) {
     const projectId = url.split('/').pop();
-    return response.redirect(308, `/gallery/${projectId}`);
+    return res.redirect(308, `/gallery/${projectId}`);
   }
 
-  // If no redirect is needed, you can pass the request to your main app
-  return response.redirect(308, '/');
+  return res.redirect(308, '/');
 }
